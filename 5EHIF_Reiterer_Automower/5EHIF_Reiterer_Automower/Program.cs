ccef5e399a921a368fdc71fda08fd80c0dcc970b
MCR <der.marcreiterer@gmail.com>
2018-09-04 22:00:29 +0200

    first work and creating the project

---
+++ 5EHIF_Reiterer_Automower/5EHIF_Reiterer_Automower/Program.cs
@@ -0,0 +1,32 @@
+﻿using BasicRenderProviders;
+using BasicScreenManagerPackage;
+using ConsoleRenderingFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5EHIF_Reiterer_Automower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // setup the GMU
+
+            int height = 50;
+            int width = 100;
+
+            GMU gmu = new GMU(height,width);
+
+            FullScreenManager screen = new FullScreenManager(height, width, gmu.PlacePixels);
+            
+            screen.App_DrawScreen(BasicProvider.getInked(10, 10, new PInfo(' ', ConsoleColor.White, ConsoleColor.White)),5,5,null);
+            gmu.PrintFrame();
+
+            Console.ReadKey(true);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # HW-1-Automower
+
+## Aufgabe
+
+Erstellen Sie einen Algorithmus fpr einen automatischen Rasenmäher. Dieser soll eine beliebige Rasenfläche so befahren, dass es zu einem möglichst gleichmäßigen Schnitt kommt. D.h. jeder Teil des Rasens wird gleichmäßig oft befahren.  
+Testen Sie den Algorithmus in einer Simulation mit einstellbaren PArametern, die die Gleichmäßigkeit des Schnitts zeigt.
+
+_
